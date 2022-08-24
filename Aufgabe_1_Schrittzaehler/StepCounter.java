ea50fedc2b2500df28018ee01f4ec3c1e96e4f31
N1kl4sK <niklas.kamm@hebk.de>
2022-08-24 11:13:25 +0200

    [FIX] Ordnername

---
+++ Aufgabe_1_Schrittzaehler/StepCounter.java
@@ -0,0 +1,14 @@
+package Aufgabe_1_Schrittzaehler;
+public class StepCounter {
+    int schritte;
+    String datumZ;
+    public StepCounter(String datum) {
+        datumZ = datum;
+    }
+    public void incrementSteps() {
+        schritte = schritte+1;
+    }
+    public String toString() {
+        return "Am " + datumZ + " bin ich " + schritte + " Schritte gegangen.";
+    }
+}
