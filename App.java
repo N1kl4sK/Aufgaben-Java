f95b2be08bf7352fad97c23a01a9b3f26f25255a
N1kl4sK <niklas.kamm@hebk.de>
2022-08-24 11:04:55 +0200

    [ADD] W.8.1 Schrittzaehler

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        StepCounter sc1 = new StepCounter("11.11.2011");
+        for (int i = 0; i < 1111; i++) {
+            sc1.incrementSteps();
+        }
+        System.out.println(sc1.toString());
+        StepCounter sc2 = new StepCounter("1.9.2017");
+        for (int i = 0; i < 10000; i++) {
+            sc2.incrementSteps();
+        }
+        System.out.println(sc2.toString());
+    }
+}
---
+++ StepCounter.java
@@ -0,0 +1,13 @@
+public class StepCounter {
+    int schritte;
+    String datumZ;
+    public StepCounter(String datum) {
+        datumZ = datum;
+    }
+    public void incrementSteps() {
+        schritte = schritte+1;
+    }
+    public String toString() {
+        return "Am " + datumZ + " bin ich " + schritte + " Schritte gegangen.";
+    }
+}
