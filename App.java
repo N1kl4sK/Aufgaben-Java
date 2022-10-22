23651854c23e29771fa72c2b3c61158f8c0ea0a1
N1kl4sK <niklas.kamm@hebk.de>
2022-10-22 12:41:09 +0200

    [ADD] Aufgabe 2

--- App.java
+++ App.java
@@ -1,6 +1,11 @@
-package Aufgabe_1_Schrittzaehler;
+import Aufgabe_1_Schrittzaehler.StepCounter;
+import Aufgabe_2_BodyMassIndex.Health;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
+        // Aufgabe 1
+
         StepCounter sc1 = new StepCounter("11.11.2011");
         for (int i = 0; i < 1111; i++) {
             sc1.incrementSteps();
@@ -11,5 +16,10 @@ public class App {
             sc2.incrementSteps();
         }
         System.out.println(sc2.toString());
+
+        // Aufgabe 2
+
+        Health h1 = new Health();
+        System.out.println(h1.bmi(57, 1.8));
     }
 }
---
+++ Aufgabe_2_BodyMassIndex/Health.java
@@ -0,0 +1,26 @@
+package Aufgabe_2_BodyMassIndex;
+
+public class Health {
+    static double bmi;
+    public static double calculateBMI(int gewicht, double groesse) {
+        bmi = gewicht / (groesse * groesse);
+        return bmi;
+    }
+    public String bmi(int gewicht, double groesse) {
+        double bmi = calculateBMI(gewicht, groesse);
+        String gewichtsklasse = "";
+        if (bmi <= 18.5) {
+            gewichtsklasse = "untergewichtig";
+        }
+        else if (bmi > 18.5 && bmi <= 25) {
+            gewichtsklasse = "normalgewichtig";
+        }
+        else if (bmi > 25 && bmi <= 30) {
+            gewichtsklasse = "übergewichtig";
+        }
+        else {
+            gewichtsklasse = "fettleibig";
+        }
+        return "Mit einem BMI von " + bmi + " sind Sie " + gewichtsklasse + ".";
+    }
+}
